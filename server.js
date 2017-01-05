09f9d524c3586d8f8d0cdc6858e850638aeb5e27
forkerino <pieterstokkink@gmail.com>
2017-01-05 14:56:26 +0000

    added logic

--- server.js
+++ server.js
@@ -3,10 +3,13 @@ let express = require('express');
 let app = express();
 
 app.get('/api/whoami', function(req, res){
-	
-	
+	let lang = req.acceptsLanguages()[0];
+	let ip = req.ip;
+	let operatingsystem = req.headers['user-agent'].split('(')[1].split(')')[0];
+	res.end(JSON.stringify({'ipaddress': ip, 'language': lang, 'OS': operatingsystem}));
 });
 
 app.listen(process.env.PORT || 5000, function(){
-	console.log('listening on port ' + process.env.PORT);
-})
+	console.log('listening on port ' + process.env.PORT || 5000);
+});
+
